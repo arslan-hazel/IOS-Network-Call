af4bebc1e7815f22ba46fa11e01b7755952906a3
Arslan Ali <arslan.ali@imaginationai.net>
2026-01-27 11:59:45 +0500

    Initial Commit

---
+++ IOS Network Call/IOS_Network_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Network_CallApp.swift
+//  IOS Network Call
+//
+//  Created by Arslan on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Network_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
